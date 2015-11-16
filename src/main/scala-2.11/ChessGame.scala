fc6903e4ec37093e0f18ab1551e1ccd2f4451fac
Mariano Gappa <mariano@movio.co>
2015-11-17 00:19:37 +1300

    Implemented move semantics and En Passant.

--- src/main/scala-2.11/ChessGame.scala
+++ src/main/scala-2.11/ChessGame.scala
@@ -1,7 +1,8 @@
 object ChessGame {
   def fromString(string: String, rules: ChessRules = ChessRules.default): ChessGame = {
     val (white, black) = (WhiteChessPlayer, BlackChessPlayer)
-    val grid = string.split('\n').mkString.zipWithIndex.toVector map {
+    val charVector = string.split('\n').mkString.zipWithIndex.toVector
+    val grid = charVector map {
       case ('♜', pos) => Some(new Rook(ChessBoard.toX(pos), ChessBoard.toY(pos), white))
       case ('♞', pos) => Some(new Knight(ChessBoard.toX(pos), ChessBoard.toY(pos), white))
       case ('♝', pos) => Some(new Bishop(ChessBoard.toX(pos), ChessBoard.toY(pos), white))
@@ -16,7 +17,15 @@ object ChessGame {
       case ('♙', pos) => Some(new Pawn(ChessBoard.toX(pos), ChessBoard.toY(pos), black, rules.whitePawnDirection * -1))
       case _ => None
     }
-    new ChessGame(new ChessBoard(grid), List(white, black), rules)
+
+    val enPassantPawns = charVector flatMap {
+      case ('↑', pos) => EnPassantPawn.fromXYDY(ChessBoard.toX(pos), ChessBoard.toY(pos), -1, grid)
+      case ('↓', pos) => EnPassantPawn.fromXYDY(ChessBoard.toX(pos), ChessBoard.toY(pos), 1, grid)
+      case _ => None
+    }
+
+    // TODO: headOption means keep only the first; this is incorrect: if there's 2 there's a problem!
+    new ChessGame(new ChessBoard(grid, enPassantPawns.headOption), List(white, black), rules)
   }
 
   val defaultGame: ChessGame = fromString(
@@ -41,48 +50,80 @@ object ChessBoard {
   val xSize = 8
   def toX = Board.toX(xSize) _
   def toY = Board.toY(xSize) _
+  def exists(x: Int, y: Int) = x >= 0 && y >= 0 && x < xSize && y < xSize
 }
 
-class ChessBoard(grid: Vector[Option[ChessPiece]]) extends Board[ChessPiece, ChessMovement, ChessBoard, ChessRules](grid, ChessBoard.xSize) {
+class ChessBoard(grid: Vector[Option[ChessPiece]], val enPassantPawn: Option[EnPassantPawn]) extends
+  Board[ChessPiece, ChessMovement, ChessBoard, ChessRules](grid, ChessBoard.xSize) {
 
   def move(m: ChessMovement)(implicit rules: ChessRules) = {
-    new ChessBoard(
-      grid.updated(fromXY(m.fromPiece.x, m.fromPiece.y), None).
-        updated(fromXY(m.fromPiece.x + m.dx, m.fromPiece.y + m.dy),
-          Some(m.fromPiece.movedTo(m.fromPiece.x + m.dx,m.fromPiece.y + m.dy)))
+    val resultingEnPassants = m match {
+      case EnPassantMovement(pawn, dx, dy) =>
+        Some(EnPassantPawn(pawn.x, pawn.y + math.signum(dy), pawn.movedTo(pawn.x, pawn.y + dy)))
+      case _ =>
+        None
+    }
+
+    val enPassantUpdate = m match {
+      case EnPassantTakeMovement(_, _, _, toPawn) => List((fromXY(toPawn.x, toPawn.y), None))
+      case _ => List()
+    }
+
+    val normalUpdates = List(
+      (fromXY(m.fromPiece.x, m.fromPiece.y), None),
+      (fromXY(m.fromPiece.x + m.dx, m.fromPiece.y + m.dy), Some(m.fromPiece.movedTo(m.fromPiece.x + m.dx,m.fromPiece.y + m.dy)))
     )
+
+    val updates = normalUpdates ++ enPassantUpdate
+    def applyUpdate(grid: Vector[Option[ChessPiece]], update: (Int, Option[ChessPiece])) = grid.updated(update._1, update._2)
+
+    new ChessBoard(updates.foldLeft(grid)(applyUpdate), resultingEnPassants)
   }
 
   def movement(fromX: Int, fromY: Int, dx: Int, dy: Int)(implicit rules: ChessRules): Option[ChessMovement] = {
     val from = get(fromX, fromY)
-    val to = get(fromX + dx, fromY + dy)
-    lazy val betweenLocationsFree = between(fromX, fromY, fromX + dx, fromY + dy) forall isEmptyCell
+    val toX = fromX + dx
+    val toY = fromY + dy
+    val to = get(toX, toY)
+    lazy val betweenLocationsFree = between(fromX, fromY, toX, toY) forall isEmptyCell
+    def isEnPassantPawn(x: Int, y: Int) = enPassantPawn.exists(epp => epp.fromX == x && epp.fromY == y)
 
-    val validateFromPiece: Option[ChessPiece] = (from, to) match {
-      case (Some(Some(p: Pawn)), Some(None))
-        if dx == 0 && betweenLocationsFree => Some(p)
+    val validateMovement: Option[ChessMovement] = (from, to, enPassantPawn) match {
+      case (Some(Some(p: Pawn)), Some(None), Some(epp: EnPassantPawn))
+        if dx != 0 && isEnPassantPawn(toX, toY) && epp.pawn.owner != p.owner =>
+          Some(EnPassantTakeMovement(p, dx, dy, epp.pawn))
 
-      case (Some(Some(p: Pawn)), Some(Some(toP: ChessPiece)))
-        if dx != 0 && (!toP.isKing || rules.kingIsTakeable) && toP.owner != p.owner => Some(p)
+      case (Some(Some(p: Pawn)), Some(None), _)
+        if dx == 0 && math.abs(dy) == 2 && betweenLocationsFree =>
+          Some(EnPassantMovement(p, dx, dy))
 
-      case (Some(Some(p: ChessPiece)), Some(None))
-        if !p.isPawn && betweenLocationsFree => Some(p)
+      case (Some(Some(p: Pawn)), Some(None), _)
+        if dx == 0 && math.abs(dy) == 1 =>
+          Some(MoveMovement(p, dx, dy))
 
-      case (Some(Some(p: ChessPiece)), Some(Some(toP: ChessPiece)))
-        if !p.isPawn && betweenLocationsFree && (!toP.isKing || rules.kingIsTakeable) && toP.owner != p.owner => Some(p)
+      case (Some(Some(p: Pawn)), Some(Some(toP: ChessPiece)), _)
+        if dx != 0 && (!toP.isKing || rules.kingIsTakeable) && toP.owner != p.owner =>
+          Some(TakeMovement(p, dx, dy, toP))
+
+      case (Some(Some(p: ChessPiece)), Some(None), _)
+        if !p.isPawn && betweenLocationsFree =>
+          Some(MoveMovement(p, dx, dy))
+
+      case (Some(Some(p: ChessPiece)), Some(Some(toP: ChessPiece)), _)
+        if !p.isPawn && betweenLocationsFree && (!toP.isKing || rules.kingIsTakeable) && toP.owner != p.owner =>
+          Some(TakeMovement(p, dx, dy, toP))
 
       case _ => None
     }
 
-    def validateMovement(fromPiece: ChessPiece): Option[ChessMovement] = {
-      val m = new ChessMovement(fromPiece, dx, dy)
+    def validateAfterMovement(m: ChessMovement): Option[ChessMovement] = {
       val newBoard = move(m)
 
       // i.e. Don't allow the movement if the moving piece's owner's King ends up threatened
-      Some(m).filter(_ => fromPiece.owner.kingPiece(newBoard).map(!_.isThreatened(newBoard)).getOrElse(true))
+      Some(m).filter(_ => m.fromPiece.owner.kingPiece(newBoard).map(!_.isThreatened(newBoard)).getOrElse(true))
     }
 
-    validateFromPiece flatMap validateMovement
+    validateMovement flatMap validateAfterMovement
   }
 
   def isDrawFor(player: ChessPlayer)(implicit rules: ChessRules) = player.movements(this).isEmpty && !isLossFor(player)
@@ -190,7 +231,17 @@ abstract class ChessPiece(x: Int, y: Int, owner: ChessPlayer) extends Piece[Ches
   val pieceName: String
 }
 
-case class ChessMovement(fromPiece: ChessPiece, dx: Int, dy: Int) extends Movement[ChessPiece](fromPiece, dx, dy)
+// TODO it's easy to implement threatens: Set[ChessPiece]
+// TODO override toString
+class ChessMovement(val fromPiece: ChessPiece, val dx: Int, val dy: Int) extends Movement[ChessPiece](fromPiece, dx, dy)
+case class TakeMovement(override val fromPiece: ChessPiece, override val dx: Int, override val dy: Int, toPiece: ChessPiece) extends ChessMovement(fromPiece, dx, dy)
+case class MoveMovement(override val fromPiece: ChessPiece, override val dx: Int, override val dy: Int) extends ChessMovement(fromPiece, dx, dy)
+case class EnPassantTakeMovement(fromPawn: Pawn, override val dx: Int, override val dy: Int, toPawn: Pawn) extends ChessMovement(fromPawn, dx, dy)
+case class EnPassantMovement(fromPawn: Pawn, override val dx: Int, override val dy: Int) extends ChessMovement(fromPawn, dx, dy)
+case class PromoteMovement(override val fromPiece: Pawn, override val dx: Int, override val dy: Int) extends ChessMovement(fromPiece, dx, dy)
+
+// TODO implement the CheckMateCase
+case class CheckMateMovement(override val fromPiece: Pawn, override val dx: Int, override val dy: Int) extends ChessMovement(fromPiece, dx, dy)
 
 class Rook(x: Int, y: Int, owner: ChessPlayer) extends ChessPiece(x, y, owner) {
   def movements(board: ChessBoard)(implicit rules: ChessRules): Set[ChessMovement] = {
@@ -254,6 +305,19 @@ class Pawn(x: Int, y: Int, owner: ChessPlayer, dy: Int) extends ChessPiece(x, y,
   def withOwner(newOwner: ChessPlayer) = new Pawn(x, y, newOwner, dy)
   def movedTo(newX: Int, newY: Int) = new Pawn(newX, newY, owner, dy)
 }
+
+object EnPassantPawn {
+  def posDy(x: Int, y: Int, dy: Int) = Board.fromXY(ChessBoard.xSize)(x, y + dy)
+
+  def fromXYDY(x: Int, y: Int, dy: Int, grid: Vector[Option[ChessPiece]]): Option[EnPassantPawn] = {
+    if (ChessBoard.exists(x, y) && ChessBoard.exists(x, y + dy)) {
+      grid(posDy(x, y, dy)) map {
+        case p: Pawn => EnPassantPawn(x, y, p)
+      }
+    } else None
+  }
+}
+case class EnPassantPawn(fromX: Int, fromY: Int, pawn: Pawn)
 
 case object WhiteChessPlayer extends ChessPlayer("White") {
   def enemy = BlackChessPlayer
