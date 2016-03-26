8f4b0ba3319d08476db697254318fc0a791cd0f7
Mariano Gappa <mariano@movio.co>
2016-03-26 15:06:03 +1300

    Removed rules from ChessPiece.cantMoveTo

--- shared/src/main/scala/ostinato/chess/core/ChessPiece.scala
+++ shared/src/main/scala/ostinato/chess/core/ChessPiece.scala
@@ -13,9 +13,17 @@ abstract class ChessPiece(pos: XY, owner: ChessPlayer) extends Piece[ChessBoard,
   val toDn: Set[String]
   val toFigurine: Char
   val pieceName: String
+  def cantMove(to: XY, board: ChessBoard): Boolean
+  def withOwner(newOwner: ChessPlayer): ChessPiece
+  protected val deltaPatterns: Set[XY]
+  protected val hasRecursiveDeltas: Boolean
 
   def isThreatened(board: ChessBoard)(implicit rules: ChessOptimisations = ChessOptimisations.default): Boolean = threatenedBy(board).nonEmpty
   def isDefended(board: ChessBoard)(implicit rules: ChessOptimisations = ChessOptimisations.default): Boolean = defendedBy(board).nonEmpty
+  val enemy: ChessPlayer = this.owner.enemy
+  def equals(that: ChessPiece) = pos == that.pos && owner == that.owner
+  override def toString = s"${owner.name}'s $pieceName on (${pos.x}, ${pos.y})"
+  def deltas(board: ChessBoard) = concreteDeltas(board, deltaPatterns, Set())
 
   def threatenedBy(board: ChessBoard)(implicit rules: ChessOptimisations = ChessOptimisations.default): Option[ChessPiece] = {
     posThreatenedBy(pos, owner, board)
@@ -30,19 +38,7 @@ abstract class ChessPiece(pos: XY, owner: ChessPlayer) extends Piece[ChessBoard,
     }
   }
 
-  def cantMove(to: XY, board: ChessBoard)(implicit rules: ChessOptimisations = ChessOptimisations.default): Boolean
-
-  val enemy: ChessPlayer = this.owner.enemy
-  def withOwner(newOwner: ChessPlayer): ChessPiece
-  def equals(that: ChessPiece) = pos == that.pos && owner == that.owner
-  override def toString = s"${owner.name}'s $pieceName on (${pos.x}, ${pos.y})"
-
-  protected val deltaPatterns: Set[XY]
-  protected val hasRecursiveDeltas: Boolean
-
-  private def concreteDeltas(board: ChessBoard, ds: Set[XY], accDeltas: Set[XY], inc: Int = 1)(
-    implicit rules: ChessOptimisations = ChessOptimisations.default): Set[XY] = {
-
+  private def concreteDeltas(board: ChessBoard, ds: Set[XY], accDeltas: Set[XY], inc: Int = 1): Set[XY] = {
     ds.map(d ⇒ (d, board.get(pos + d * inc))).flatMap[XY, Set[XY]] {
       case (d: XY, Some(None)) if hasRecursiveDeltas ⇒ concreteDeltas(board, Set(d), accDeltas + (d * inc), inc + 1)
       case (d: XY, Some(None)) ⇒ accDeltas + (d * inc)
@@ -52,17 +48,23 @@ abstract class ChessPiece(pos: XY, owner: ChessPlayer) extends Piece[ChessBoard,
     }
   }
 
-  def deltas(board: ChessBoard)(implicit rules: ChessOptimisations = ChessOptimisations.default) =
-    concreteDeltas(board, deltaPatterns, Set())
-
-  def actions(board: ChessBoard)(implicit rules: ChessOptimisations = ChessOptimisations.default): Set[ChessAction] = {
-    deltas(board).flatMap { case delta ⇒ movementsOfDelta(pos, delta, board) }
+  def actions(board: ChessBoard)(implicit rules: ChessOptimisations = ChessOptimisations.default): Set[ChessAction] = deltas(board).flatMap {
+     case delta ⇒ movementsOfDelta(pos, delta, board)
   }
 }
 
 case class ♜(override val pos: XY, override val owner: ChessPlayer) extends ChessPiece(pos, owner) {
   protected val hasRecursiveDeltas = true
   protected val deltaPatterns = Piece.toXYs(Set((-1, 0), (1, 0), (0, -1), (0, 1)))
+  val pieceName = "Rook"
+  val toAn = "R"
+  val toDn = Set("R", "KR", "QR")
+  lazy val toIccf = 2
+  val toFen = if (owner == WhiteChessPlayer) toAn.head else toAn.head.toLower
+  override val isRook = true
+  def withOwner(newOwner: ChessPlayer) = ♜(pos, newOwner)
+  def movedTo(newXY: XY) = ♜(newXY, owner)
+  override def cantMove(to: XY, board: ChessBoard) = pos.x != to.x && pos.y != to.y
 
   lazy val castlingSide =
     if (pos.x == 0)
@@ -76,28 +78,11 @@ case class ♜(override val pos: XY, override val owner: ChessPlayer) extends Ch
     case BlackChessPlayer ⇒ '♜'
     case WhiteChessPlayer ⇒ '♖'
   }
-
-  val pieceName = "Rook"
-  val toAn = "R"
-  val toDn = Set("R", "KR", "QR")
-  lazy val toIccf = 2
-  val toFen = if (owner == WhiteChessPlayer) toAn.head else toAn.head.toLower
-  override val isRook = true
-  def withOwner(newOwner: ChessPlayer) = ♜(pos, newOwner)
-  def movedTo(newXY: XY) = ♜(newXY, owner)
-  override def cantMove(to: XY, board: ChessBoard)(implicit rules: ChessOptimisations = ChessOptimisations.default) =
-    pos.x != to.x && pos.y != to.y
 }
 
 case class ♝(override val pos: XY, override val owner: ChessPlayer) extends ChessPiece(pos, owner) {
   protected val hasRecursiveDeltas = true
   protected val deltaPatterns = Piece.toXYs(Set((-1, -1), (1, 1), (-1, 1), (1, -1)))
-
-  val toFigurine = owner match {
-    case BlackChessPlayer ⇒ '♝'
-    case WhiteChessPlayer ⇒ '♗'
-  }
-
   val pieceName = "Bishop"
   val toAn = "B"
   val toDn = Set("B", "KB", "QB")
@@ -106,19 +91,17 @@ case class ♝(override val pos: XY, override val owner: ChessPlayer) extends Ch
   override val isBishop = true
   def withOwner(newOwner: ChessPlayer) = ♝(pos, newOwner)
   def movedTo(newXY: XY) = ♝(newXY, owner)
-  override def cantMove(to: XY, board: ChessBoard)(implicit rules: ChessOptimisations = ChessOptimisations.default) =
-    (pos - to).abs.subtractXY != 0
+  override def cantMove(to: XY, board: ChessBoard) = (pos - to).abs.subtractXY != 0
+
+  val toFigurine = owner match {
+    case BlackChessPlayer ⇒ '♝'
+    case WhiteChessPlayer ⇒ '♗'
+  }
 }
 
 case class ♞(override val pos: XY, override val owner: ChessPlayer) extends ChessPiece(pos, owner) {
   protected val hasRecursiveDeltas = false
   protected val deltaPatterns = Piece.toXYs(Set((-1, -2), (1, -2), (-1, 2), (1, 2), (-2, -1), (-2, 1), (2, -1), (2, 1)))
-
-  val toFigurine = owner match {
-    case BlackChessPlayer ⇒ '♞'
-    case WhiteChessPlayer ⇒ '♘'
-  }
-
   val pieceName = "Knight"
   val toAn = "N"
   val toDn = Set("N", "KN", "QN")
@@ -127,19 +110,17 @@ case class ♞(override val pos: XY, override val owner: ChessPlayer) extends Ch
   override val isKnight = true
   def withOwner(newOwner: ChessPlayer) = ♞(pos, newOwner)
   def movedTo(newXY: XY) = ♞(newXY, owner)
-  override def cantMove(to: XY, board: ChessBoard)(implicit rules: ChessOptimisations = ChessOptimisations.default) =
-    deltas(board).forall(pos + _ != to)
+  override def cantMove(to: XY, board: ChessBoard) = deltas(board).forall(pos + _ != to)
+
+  val toFigurine = owner match {
+    case BlackChessPlayer ⇒ '♞'
+    case WhiteChessPlayer ⇒ '♘'
+  }
 }
 
 case class ♛(override val pos: XY, override val owner: ChessPlayer) extends ChessPiece(pos, owner) {
   protected val hasRecursiveDeltas = true
   protected val deltaPatterns = Piece.toXYs(Set((-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, 1), (-1, 1), (1, -1)))
-
-  val toFigurine = owner match {
-    case BlackChessPlayer ⇒ '♛'
-    case WhiteChessPlayer ⇒ '♕'
-  }
-
   val pieceName = "Queen"
   val toAn = "Q"
   val toDn = Set("Q")
@@ -148,8 +129,12 @@ case class ♛(override val pos: XY, override val owner: ChessPlayer) extends Ch
   override val isQueen = true
   def withOwner(newOwner: ChessPlayer) = ♛(pos, newOwner)
   def movedTo(newXY: XY) = ♛(newXY, owner)
-  override def cantMove(to: XY, board: ChessBoard)(implicit rules: ChessOptimisations = ChessOptimisations.default) =
-    (pos - to).abs.subtractXY != 0 && pos.x != to.x && pos.y != to.y
+  override def cantMove(to: XY, board: ChessBoard) = (pos - to).abs.subtractXY != 0 && pos.x != to.x && pos.y != to.y
+
+  val toFigurine = owner match {
+    case BlackChessPlayer ⇒ '♛'
+    case WhiteChessPlayer ⇒ '♕'
+  }
 }
 
 object ♚ {
@@ -172,12 +157,6 @@ case class ♚(override val pos: XY, override val owner: ChessPlayer) extends Ch
   def targetRookPosition(dx: Int) = XY(if (dx < 0) 0 else 7, initialY)
   def rookDeltaFor(kingDelta: XY) = ♚.rookDelta(kingDelta)
   lazy val isInInitialPosition = pos.x == initialX && pos.y == initialY
-
-  val toFigurine = owner match {
-    case BlackChessPlayer ⇒ '♚'
-    case WhiteChessPlayer ⇒ '♔'
-  }
-
   val pieceName = "King"
   val toAn = "K"
   val toDn = Set("K")
@@ -186,8 +165,12 @@ case class ♚(override val pos: XY, override val owner: ChessPlayer) extends Ch
   override val isKing = true
   def withOwner(newOwner: ChessPlayer) = ♚(pos, newOwner)
   def movedTo(newXY: XY) = ♚(newXY, owner)
-  override def cantMove(to: XY, board: ChessBoard)(implicit rules: ChessOptimisations = ChessOptimisations.default) =
-    pos.chebyshevDistance(to) > 1
+  override def cantMove(to: XY, board: ChessBoard) = pos.chebyshevDistance(to) > 1
+
+  val toFigurine = owner match {
+    case BlackChessPlayer ⇒ '♚'
+    case WhiteChessPlayer ⇒ '♔'
+  }
 }
 case class ♟(override val pos: XY, override val owner: ChessPlayer, dy: Int) extends ChessPiece(pos, owner) {
   val isInInitialPosition = dy == 1 && pos.y == 1 || dy == -1 && pos.y == 6
@@ -198,12 +181,6 @@ case class ♟(override val pos: XY, override val owner: ChessPlayer, dy: Int) e
   def promotingPosition(dy: Int) = Map(-1 -> 0, 1 -> 7)(dy)
   val distanceToPromotion = math.abs(pos.y - promotingPosition(dy))
   val isPromoting = pos.y == promotingPosition(dy)
-
-  val toFigurine = owner match {
-    case BlackChessPlayer ⇒ '♟'
-    case WhiteChessPlayer ⇒ '♙'
-  }
-
   val pieceName = "Pawn"
   val toAn = ""
   val toDn = Set("P")
@@ -212,8 +189,12 @@ case class ♟(override val pos: XY, override val owner: ChessPlayer, dy: Int) e
   override val isPawn = true
   def withOwner(newOwner: ChessPlayer) = ♟(pos, newOwner, dy)
   def movedTo(newXY: XY) = ♟(newXY, owner, dy)
-  override def cantMove(to: XY, board: ChessBoard)(implicit rules: ChessOptimisations = ChessOptimisations.default) =
-    deltas(board).forall(pos + _ != to)
+  override def cantMove(to: XY, board: ChessBoard) = deltas(board).forall(pos + _ != to)
+
+  val toFigurine = owner match {
+    case BlackChessPlayer ⇒ '♟'
+    case WhiteChessPlayer ⇒ '♙'
+  }
 }
 
 object EnPassantPawn {
