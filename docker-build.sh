a2ac4d25e4309907e8e1302d3467e3f09b46121c
Mariano Gappa <mariano@movio.co>
2017-03-11 20:24:44 +1300

    Simplifies docker-build.sh. Adds check.

--- docker-build.sh
+++ docker-build.sh
@@ -4,43 +4,35 @@ set -e
 
 PROJECT_VERSION=$(./version.sh)
 PROJECT_SCALA_VERSION=$(./scalaMajorVersion.sh)
-
-NAME="ostinato"
-ZIP_NAME="${NAME}-${PROJECT_VERSION}"
+ZIP_NAME="ostinato-${PROJECT_VERSION}"
 ZIP_PATH="target/${ZIP_NAME}"
-
-# Docker image name and tag
-IMAGE_NAME=$1
-TAG=$2
+DOCKER_TAG=${TAG-latest}
 
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $SCRIPT_DIR
-
-if [ -z "$IMAGE_NAME" ] ; then
-	cat << 'EOF'
-
-  Usage:  ./docker-build.sh <image-name> <tag>
-  e.g.:   ./docker-build.sh ostinato-server test
-
-EOF
-	exit 1
-fi
 
 if [[ ! -f "${ZIP_PATH}.zip" ]]; then
     echo >&2 "Please run 'sbt packArchiveZip' first."
     exit 1
 fi
 
+if [[ $(find . -name "${ZIP_NAME}.zip" -mmin -30 | wc -l) -eq 0 ]]; then
+    echo >&2 "Please run 'sbt packArchiveZip' again; zip file exists but it was last modified more than a half hour ago. Otherwise hack me."
+    exit 1
+fi
+
+if [[ "$DOCKER_TAG" == "latest" ]]; then
+    echo >&2 "Building docker tag [latest]. For custom tag use e.g.: TAG=v1.0.0 ./docker-build.sh"
+fi
+
 find $SCRIPT_DIR -name "${ZIP_NAME}.zip" -exec unzip -o {} -d target/ \;
 mkdir -p target/pack/lib
 mv -f -v target/${ZIP_NAME}/lib/* "target/pack/lib"
 
-#TODO remove sjs files
 rm target/pack/lib/*_sjs*.jar
 
-echo "Moving $IMAGE_NAME jar out of lib folder so that there are less changes to be added to the docker image"
-mv target/pack/lib/$NAME*.jar target/pack/$NAME.jar
+echo "Moving ostinato jar out of lib folder so that there are less changes to be added to the docker image"
+mv target/pack/lib/ostinato*.jar target/pack/ostinato.jar
 
-
-echo "Creating the docker image ${IMAGE_NAME}:${TAG}"
-docker build --quiet=false -t $IMAGE_NAME:$TAG .
+echo "Creating the docker image ostinato:${DOCKER_TAG}"
+docker build --quiet=false -t ostinato:$DOCKER_TAG .
